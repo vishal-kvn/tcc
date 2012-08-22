04adfe6c2d8b9e9af67915fa4b8985e182c8ed0f
vishal kajjam <vishal.kvn@gmail.com>
2012-08-22 05:55:50 -0400

    modified location method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -19,19 +19,22 @@ class HomeController < ApplicationController
 		# need to check for different responses
 		# current location - ip based look up
 		# given location
-		debugger
+		# debugger
 		@ip_address = (request.ip=="127.0.0.1") ? "69.142.67.13" : request.ip#request.ip
 		@geo_location = Geocoder.search(params[:search] || @ip_address)
+		@location = nil
 
 		if !@geo_location.empty?
 			if params[:search]
 				# @geo_location = Geocoder.search(params[:search])
 				@latitude = @geo_location[0].data["geometry"]["location"]["lat"]
 				@longitude = @geo_location[0].data["geometry"]["location"]["lng"]
+				@location = @geo_location[0].formatted_address
 
 			else
 				@latitude = @geo_location[0].latitude
 				@longitude = @geo_location[0].longitude
+				@location = @geo_location[0].data["city"] + " " + @geo_location[0].data["region_code"] + "(Current Location)"
 				# [@geo_location[0].latitude, @geo_location[0].longitude]
 
 			end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,13 @@
 module ApplicationHelper
-	def clocation
-		if @geo_location[0].class == Geocoder::Result::Google
-			@geo_location[0].formatted_address
-		elsif @geo_location[0].class == Geocoder::Result::Freegeoip 
-			@geo_location[0].data["city"] + " " + @geo_location[0].data["region_code"] + "(Current Location)"
-		else
-			"Undisclosed Location"
-		end
-	end
+	# def clocation
+	# 	if @geo_location[0].kind_of? Geocoder::Result::Google
+	# 		@geo_location[0].formatted_address
+	# 	elsif @geo_location[0].kind_of? Geocoder::Result::Freegeoip 
+	# 		@geo_location[0].data["city"] + " " + @geo_location[0].data["region_code"] + "(Current Location)"
+	# 	else
+	# 		"Undisclosed Location"
+	# 	end
+
+		# (@geo_location[0].formatted_address || (@geo_location[0].data["city"] + " " + @geo_location[0].data["region_code"] + "(Current Location)") || "Undisclosed Location")
+	# end
 end
